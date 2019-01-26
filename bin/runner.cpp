b87ea8dbc2394d43bff3214502d5312bf00b411e
coord.e <me@coord-e.com>
2019-01-26 12:51:35 +0000

    Fix: Call destructor properly after ctrl-c or something like that

--- bin/runner.cpp
+++ bin/runner.cpp
@@ -4,16 +4,27 @@
 #include <chrono>
 #include <cstdint>
 #include <thread>
+#include <atomic>
+#include <signal.h>
 
 #include <flom/flom.hpp>
 #include <servoarray/servoarray.h>
 #include <servoarray/servomap.h>
+
+static std::atomic<bool> quit(false);
+
+void register_signal(int);
+void quit_handler(int);
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     std::cerr << "Usage: " << argv[0] << " INPUT" << std::endl;
     return -1;
   }
+
+  register_signal(SIGINT);
+  register_signal(SIGQUIT);
+  register_signal(SIGTERM);
 
   std::ifstream f(argv[1], std::ios::binary);
   auto const motion = flom::Motion::load(f);
@@ -25,7 +36,20 @@ int main(int argc, char *argv[]) {
     for (auto const& [name, pos] : frame.positions()) {
       servos.write(name, pos);
     }
+
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+    if(quit.load()) break;
   }
 }
 
+void register_signal(int signal) {
+  struct sigaction action{};
+  action.sa_handler = quit_handler;
+  sigfillset(&action.sa_mask);
+  sigaction(signal, &action, nullptr);
+}
+
+void quit_handler(int) {
+  quit.store(true);
+}
