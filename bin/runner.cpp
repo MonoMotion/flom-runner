70fb8349e3dac11ef6ce13d4e6b18bb6504529fb
coord.e <me@coord-e.com>
2019-02-06 09:49:01 +0000

    Change: Rewrite argument processing with args

--- bin/runner.cpp
+++ bin/runner.cpp
@@ -12,14 +12,33 @@
 #include <servoarray/servoarray.h>
 #include <servoarray/servomap.h>
 
+#include <args.hxx>
+
 static std::atomic<bool> quit(false);
 
 void register_signal(int);
 void quit_handler(int);
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    std::cerr << "Usage: " << argv[0] << " INPUT [FPS=0.01]" << std::endl;
+  args::ArgumentParser argparser("Run flom motion file on real robot");
+  args::HelpFlag help(argparser, "help", "Print this help", {'h', "help"});
+  args::Positional<std::string> arg_motion(argparser, "motion", "motion file");
+  args::ValueFlag<double> arg_fps(argparser, "fps", "fps", {'f', "fps"});
+
+  try {
+    argparser.ParseCLI(argc, argv);
+  } catch (args::Help){
+    std::cout << argparser;
+    return 0;
+  } catch (args::ParseError e){
+    std::cerr << e.what() << std::endl;
+    std::cerr << argparser;
+    return -1;
+  }
+
+  if(!arg_motion) {
+    std::cerr << "Specify the motion file to play" << std::endl;
+    std::cerr << argparser;
     return -1;
   }
 
@@ -27,10 +46,10 @@ int main(int argc, char *argv[]) {
   register_signal(SIGQUIT);
   register_signal(SIGTERM);
 
-  std::ifstream f(argv[1], std::ios::binary);
+  std::ifstream f(args::get(arg_motion), std::ios::binary);
   auto const motion = flom::Motion::load(f);
 
-  double const fps = argc > 2 ? std::atof(argv[2]) : 0.01;
+  double const fps = arg_fps ? args::get(arg_fps) : 0.01;
 
   auto array = ServoArray::ServoArray();
   auto servos = ServoArray::ServoMap(array);
